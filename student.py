8a7bec848e19503ab1a1c04363cd00ddced0b7b0
Denis Mironov <mds.myst@gmail.com>
2024-11-11 19:28:12 +0400

    add class Ticket and main.py, fix  all class

--- student.py
+++ student.py
@@ -5,7 +5,7 @@ class GradeTooHighException(Exception):
     def __init__(self, grade):
         self.grade = grade
         super().__init__(f"Grade {self.grade} is not allowed. It must be 5 or below.")
-    pass
+
 
 class Student(Person):
     MAX_MARK = 5
