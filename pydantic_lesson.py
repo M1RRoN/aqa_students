db3a92256e7e2d7e3b17c34e278863e239188e5e
Denis Mironov <mds.myst@gmail.com>
2025-01-27 20:06:24 +0400

    fix pydantic

--- pydantic_lesson.py
+++ pydantic_lesson.py
@@ -1,5 +1,3 @@
-from typing import ClassVar
-
 from pydantic import BaseModel, Field, model_validator, ConfigDict
 
 MIN_MARK = 2
@@ -7,18 +5,18 @@ MAX_MARK = 5
 
 
 class GroupStatistics(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+
     avg_mark: float = Field(alias="avgMark", ge=MIN_MARK, le=MAX_MARK)
     min_mark: float = Field(alias="minMark", ge=MIN_MARK, le=MAX_MARK)
     max_mark: float = Field(alias="maxMark", ge=MIN_MARK, le=MAX_MARK)
-
-    model_config = ConfigDict(populate_by_name=True)
 
     @model_validator(mode="after")
     def check_avg_mark(cls, model: "GroupStatistics"):
         if model.min_mark > model.max_mark:
             raise ValueError("min_mark не может быть больше max_mark")
         if not (model.min_mark <= model.avg_mark <= model.max_mark):
-            raise ValueError("avg_mark должно быть между min_mark и max_mark")
+            raise ValueError("avg_mark должно быть между min_mark и max_mark (включительно)")
         return model
 
 
